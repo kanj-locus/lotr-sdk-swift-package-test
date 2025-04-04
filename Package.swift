fd57a08a50fb7c5bfb977ddc8c391b35c5617930
kanj-locus <kanj@locus.sh>
2025-04-04 17:47:30 +0530

    Try wrapper approach

--- Package.swift
+++ Package.swift
@@ -13,14 +13,6 @@ let package = Package(
         .library(name: "LotrSdk", targets: ["LotrSdk"]),
         .library(name: "LotrModels", targets: ["LotrModels"])
     ],
-    dependencies: [
-        .package(url: "https://github.com/Alamofire/Alamofire", from: "5.9.0"),
-        .package(url: "https://github.com/Moya/Moya", from: "15.0.0"),
-        .package(url: "https://github.com/ReactiveX/RxSwift", from: "6.9.0"),
-        .package(url: "https://github.com/RxSwiftCommunity/RxCoreLocation", from: "1.5.1"),
-        .package(url: "https://github.com/realm/realm-swift", from: "10.54.3"),
-        .package(url: "https://github.com/RxSwiftCommunity/RxRealm", from: "5.0.7"),
-    ],
     targets: [
         // Targets are the basic building blocks of a package, defining a module or a test suite.
         // Targets can depend on other targets in this package and products from dependencies.
